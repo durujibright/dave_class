10bb65fdd9ff47b6a6a6d5310e963fad5d27f659
Bright <duruji.b.ifeanyi@gmail.com>
2022-08-15 11:42:24 +0100

    done

---
+++ script.js
@@ -0,0 +1,87 @@
+var firstName = document.getElementById("Firstname")
+var lastName = document.getElementById("Lastname")
+var userName = document.getElementById("Username")
+var email = document.getElementById("Email")
+var password = document.getElementById("Password")
+var confirm_password = document.getElementById("Confirm_password")
+var submit = document.getElementById("  ")
+
+
+// function validate() {
+
+//     if ( firstName === null ) {
+//         alert("firstname can't be blank")
+//         return false;
+//     }
+
+
+//     if ( lastName === null ) {
+//         alert("last name can't be blank")
+//         return false;
+//     }
+
+
+//     if ( userName === null ) {
+//         alert("provide a unique username")
+//         return false;
+//     }
+
+
+//     if ( email === null ) {
+//         alert("email is required")    
+//         return false;
+//     }   
+//     //else if (email.indexof("@") !== -1){
+//     //     alert("provide a valid email address")
+//     //     return false;
+//     // }
+
+
+//     if ( password === " " && password <= 7) {
+//        alert("must not be less than 8 charaters")      
+//        return false;
+//     }
+
+
+//     if ( confirm_password !== password ) {
+//         alert("password are not the same")
+//         return false;
+//     }
+//     return (false);
+// }
+
+
+function validate() {
+
+    if (document.myForm.Firstname.value == "") {
+        alert("Please provide your First name!");
+        document.myForm.Firstname.focus();
+        return false;
+    }
+    if (document.myForm.Lastname.value == "") {
+        alert("Please provide your Last name!");
+        document.myForm.Lastname.focus();
+        return false;
+    }
+    if (document.myForm.Username.value == "") {
+        alert("Please provide your User name!");
+        document.myForm.Username.focus();
+        return false;
+    }
+    if (document.myForm.Email.value == "") {
+        alert("Please a valid Email!");
+        document.myForm.Email.focus();
+        return false;
+    }
+    if (document.myForm.Password.value === "  " || document.myForm.Password.value.length <= 8) {
+        alert("Input a strong Combination for your passcode.");
+        document.myForm.Password.focus();
+        return false;
+    }
+    if (document.myForm.Confirm_password.value !== Password) {
+        alert("your password dosent match");
+        document.myForm.Confirm_password.focus();
+        return (true);
+    }
+    return (true);
+}
